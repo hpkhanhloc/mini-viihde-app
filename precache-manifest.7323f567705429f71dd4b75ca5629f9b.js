2334aca4e3fda5c1383cc8d1b3fb97e002c5bb35
Loc Hoang <loc.hoang@aalto.fi>
2020-02-25 16:22:07 +0700

    Updates

---
+++ precache-manifest.7323f567705429f71dd4b75ca5629f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8673230be65f5b92daba3e50fc1b638d",
+    "url": "/mini-viihde-app/index.html"
+  },
+  {
+    "revision": "0a82b89f219fc48cc6e7",
+    "url": "/mini-viihde-app/static/css/main.8913a5b7.chunk.css"
+  },
+  {
+    "revision": "b5af791b92e0fe9665ac",
+    "url": "/mini-viihde-app/static/js/2.b22c37b8.chunk.js"
+  },
+  {
+    "revision": "6a43b004aa4e5442f43c089cbe637e64",
+    "url": "/mini-viihde-app/static/js/2.b22c37b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a82b89f219fc48cc6e7",
+    "url": "/mini-viihde-app/static/js/main.03a307a2.chunk.js"
+  },
+  {
+    "revision": "bc1afe467a80132b7786",
+    "url": "/mini-viihde-app/static/js/runtime-main.d31cd912.js"
+  }
+]);
